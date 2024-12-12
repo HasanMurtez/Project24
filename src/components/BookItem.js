ebbf3c1861b59bddb3727c64b45eb9b9ca29da44
Hasan Murtaza <g00419888@atu.ie>
2024-12-12 20:18:45 +0000

    displaying all book details

--- src/components/BookItem.js
+++ src/components/BookItem.js
@@ -1,13 +1,23 @@
 const BookItem = (props) => {
-  const { title, author, year } = props.myBook;
+  const { title, author, year, genre, poster } = props.myBook;
 
   return (
-    <div>
+    <div style={{ border: '1px solid #ddd', padding: '10px', marginBottom: '10px' }}>
       <h4>{title}</h4>
-      <p>
-        <strong>Author:</strong> {author} <br />
-        <strong>Year:</strong> {year}
-      </p>
+      <p><strong>Author:</strong> {author}</p>
+      <p><strong>Year:</strong> {year}</p>
+      <p><strong>Genre:</strong> {genre}</p>
+      {poster && (
+        <div>
+          <strong>Poster:</strong>
+          <br />
+          <img
+            src={poster}
+            alt={`${title} poster`}
+            style={{ maxWidth: '150px', maxHeight: '200px', marginTop: '10px' }}
+          />
+        </div>
+      )}
     </div>
   );
 };
